0063e9046231ea66751c2eabfec581b2edf42aa7
Anurag-builds <anurag.official135@gmail.com>
2025-10-05 12:16:10 +0530

    exceptions done starting collections

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 
-namespace Program
+namespace ConsoleApp7
 {
-    class Car
+    class InsufficientFundException(string message) : Exception(message)
     {
-        public void StartCar()
+    }
+    class BankAccount
+    {
+        public double Balance { get; set; } = 1000;
+        public void Withdraw(double amount)
         {
-            Console.WriteLine("Wrommmmmm.....");
-        }
-        public void StopCar()
-        {
-            Console.WriteLine("Brrrr.....");
+            if (amount > Balance)
+            {
+                throw new InsufficientFundException("Not enough Balance");
+
+            }
+            Balance -= amount;
         }
     }
 
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new();
+            try
+            {
+                account.Withdraw(3000);
+                Console.WriteLine("withdraw Complete");
+            }
+            catch (InsufficientFundException e)
+            {
+                Console.WriteLine("Transaction Failed " + e.Message);
+            }
+
+        }
+    }
+
+
+
 }
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Program
+{
+    class PasswordLengthExeception : Exception
+    {
+        public PasswordLengthExeception() : base("Password Length must be greater than or equal to 6"){ }
+    }
+    class SpecialCharException : Exception
+    {
+        public SpecialCharException(): base("Password must contain at least one special character"){ }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+
+                if (password.Length < 6)
+
+                    throw new PasswordLengthExeception();
+
+                if (!(password.Contains("!") || password.Contains("@") ||
+                    password.Contains("#") || password.Contains("$") ||
+                    password.Contains("%") || password.Contains("^") ||
+                    password.Contains("&") || password.Contains("*") ||
+                    password.Contains("(") || password.Contains(")")))
+                    throw new SpecialCharException();
+                Console.WriteLine("Valid Password");
+            }
+            catch (PasswordLengthExeception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (SpecialCharException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
